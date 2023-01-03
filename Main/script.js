55ed27ece77f85df63490eea5251f6539ceff9c7
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-03 04:14:28 -0600

    added class

--- Main/script.js
+++ Main/script.js
@@ -4,6 +4,20 @@ $('#currentDay').text(moment().format('dddd, MMMM Do'));
 $(document).ready(function(){
 
     $("saveBtn").on("click", function(){
+        
+   var value = $(this).siblings("textEvent").val();
+    var time = $(this).parent().attr('id');
+
+    
+    localStorage.setItem(time, value);
+
+  
+    $('.notification').addClass('show');
+
+    
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
        
       });
 
