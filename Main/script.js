bc08607bcd724254cc7749a1edb60da880e98945
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-03 04:45:46 -0600

    added hour block

--- Main/script.js
+++ Main/script.js
@@ -12,15 +12,37 @@ $(document).ready(function(){
     localStorage.setItem(time, value);
 
   
-    $('.notification').addClass('show');
+    $(".bulletin").addClass("show");
 
     
     setTimeout(function () {
-      $('.notification').removeClass('show');
+      $(".bulletin").removeClass("show");
     }, 5000);
        
       });
+    });
 
+    function timeUpdater() {
+        
+        var currentHour = moment().hours();
+    
+       
+        $(".time-block").each(function () {
+          var blockHour = parseInt($(this).attr('id').split('-')[1]);
+    
+          
+          if (blockHour < currentHour) {
+            $(this).addClass('past');
+          } else if (blockHour === currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+          } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+          }
+        });
+      }
+    
+      timeUpdater();
 
-
-});
